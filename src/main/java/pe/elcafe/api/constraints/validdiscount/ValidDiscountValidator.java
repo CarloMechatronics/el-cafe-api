0b0de996e3cd6a82775e86a008e35d1008977792
CarloMechatronics <carlo.torres@utec.edu.pe>
2024-07-03 16:59:04 -0500

    AtLeastOneNotNull constraint created

--- src/main/java/pe/elcafe/api/constraints/validdiscount/ValidDiscountValidator.java
+++ src/main/java/pe/elcafe/api/constraints/validdiscount/ValidDiscountValidator.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 public class ValidDiscountValidator implements ConstraintValidator<ValidDiscount, BigDecimal> {
     @Override
-    public boolean isValid(BigDecimal discount, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(BigDecimal discount, ConstraintValidatorContext context) {
         if (discount == null)
             return true;
         return discount.compareTo(BigDecimal.ZERO) >= 0 && discount.compareTo(BigDecimal.valueOf(80)) <= 0;
