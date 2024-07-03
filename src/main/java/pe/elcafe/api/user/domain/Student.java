3845e957478597595d7ff8f6f7dbb3c495058b70
CarloMechatronics <carlo.torres@utec.edu.pe>
2024-07-02 23:58:33 -0500

    Student entity updated with new relations

--- src/main/java/pe/elcafe/api/user/domain/Student.java
+++ src/main/java/pe/elcafe/api/user/domain/Student.java
@@ -1,12 +1,16 @@
 package pe.elcafe.api.user.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pe.elcafe.api.purchaseorder.domain.PurchaseOrder;
+import pe.elcafe.api.shoppingcart.domain.ShoppingCart;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -17,6 +21,12 @@ public class Student extends UserAccount {
     @Size(min = 7, max = 15)
     @Column(unique = true, nullable = false, length = 15)
     private String studentCode;
+
+    @OneToOne(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private ShoppingCart shoppingCart;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<PurchaseOrder> purchaseOrders = new ArrayList<>();
 
     public Student(String studentCode, String password, String firstName, String middleName, String lastName, String email, String phone) {
         super.setUsername(studentCode);
